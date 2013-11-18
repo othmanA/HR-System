d11abc5b957c89372af9e23f927c48405ef998bc
apu1989 <mach3238@stthomas.edu>
2013-11-18 15:13:00 -0600

    Added methods to Job class
    Have an error that needs to be fixed.

--- models/Job.cs
+++ models/Job.cs
@@ -7,28 +7,49 @@ namespace HR
 {
     public class Job
     {
-        private bool workingStatus { get; set; }
+        private bool workingStatus;
         private String contract { get; set; }
         private int hoursPerDay { get; set; }
-        private DateTime firstDayAtWork { get; set; }
+        private DateTime firstDayAtWork;
 
         public Job(int status, String contract, int hoursPDay, String firstDay)
         {
-            if (status == 0)
-            {
-                bool workingStatus = true;
-            }
+            // the contract and hoursPerDay should be set from here
+            this.hoursPerDay = hoursPDay;
+            this.contract = contract;
+
+            // use the setter to set the date
+            setFirstDayAtWork(firstDay);
+
+            // use the setter to set the boolean value
+            if (status == 1)
+                setWorkingStatus(true);
             else
-            {
-                bool workingStatus = false;
-            }
-
-
-            this.firstDayAtWork = DateTime.Parse(firstDay);
-
-
+                setWorkingStatus(false);
         }
 
+        public void setFirstDayAtWork(String firstDayAtWork)
+        {
+            this.firstDayAtWork = DateTime.Parse(firstDayAtWork);
+        }
 
+        // The value coming from the database is 1 or 0, convert it to boolean
+        public void setWorkingStatus(bool status)
+        {
+            this.workingStatus = status;
+        }
+
+        // And create your getter
+        // Because if we created our setter we need also to create the getter
+
+        public string getFirstDayAtWork()
+        {
+            return this.firstDayAtWork.toString();
+        }
+
+        public bool getWorkingStatus()
+        {
+            return this.status;
+        }
     }
 }

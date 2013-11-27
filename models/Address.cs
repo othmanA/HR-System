4bdfeea155963bc86c6f6e6eedeba7560551d1e7
othmanA <abah4506@stthomas.edu>
2013-11-27 06:00:35 -0600

    Getting an Employee From the database
    - Get any employee from the database by id or SSN
    - SETTERS AND GETTERS
    - the address and job are created by the employee object.
    - Next step is to create a new employee based on the current properties.
    
    - AND update the employee based on the current properties.

--- models/Address.cs
+++ models/Address.cs
@@ -13,5 +13,36 @@ namespace HR
         private string state;
         private int zipCode;
 
+        public Address(string a1, string a2, string city, string state, int zipCode) {
+            this.address1 = a1;
+            this.address2 = a2;
+            this.state = state;
+            this.city = city;
+            this.zipCode = zipCode;
+        }
+
+        public string getAddress1() {
+            return this.address1;
+        }
+
+        public string getAddress2()
+        {
+            return this.address2;
+        }
+
+        public string getCity()
+        {
+            return this.city;
+        }
+
+        public string getState()
+        {
+            return this.state;
+        }
+
+        public int getZipCode()
+        {
+            return this.zipCode;
+        }
     }
 }
--- models/Employee.cs
+++ models/Employee.cs
@@ -10,27 +10,18 @@ namespace HR
     {
 
         private int id;
-        private int SSN;
+        private string SSN;
         private string firstName;
         private char middleInitial;
         private string lastName;
         private DateTime dob;
         private char gender;
-        private int approved;
+        private bool approved;
         private DateTime created_at;
         private string phone;
 
         private Job job;
         private Address address;
-
-        /**
-         * Constructor
-         * 
-         */
-        public Employee() {
-          
-        }
-
 
         /**
          * Find By ID
@@ -51,28 +42,57 @@ namespace HR
         }
 
 
+        /**
+         * Find By SSN
+         * 
+         * This method should take the SSN as an argument and get the employee from database 
+         * 
+         */
+        public void findBySSN(int SSN)
+        {
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.setSQL("SELECT * FROM Employee WHERE employee_SSN = @SSN");
+            handler.addParameter("@SSN", SSN.ToString());
+            handler.queryExecute();
+
+            while (handler.reader.Read())
+            {
+                // Sending all the data to the big setter
+                setObject(handler.reader);
+            }
+        }
+
+
 
         /**
          * This method should set the data from any SQL reader
          */
         private void setObject(SqlDataReader r)
         {
+            this.id = int.Parse(r["employee_id"].ToString());
+            this.SSN = r["employee_SSN"].ToString();
             this.firstName = r["employee_firstName"].ToString();
+            this.lastName = r["Employee_lastName"].ToString();
+            this.dob = DateTime.Parse(r["employee_dob"].ToString());
+            this.created_at = DateTime.Parse(r["employee_created_at"].ToString());
+            this.phone = r["employee_phone"].ToString();
 
-            // Converting the object to char
-            string m = r["employee_middleInitial"].ToString();
+
+            // Converting the object to char -- Middle Initial 
+            string m = r["employee_middleInital"].ToString();
             this.middleInitial = m[0];
 
-            this.lastName = r["Employee_lastName"].ToString();
-            this.dob = DateTime.Parse(r["employee_firstName"].ToString());
+
 
             // converting the object ot char (gender)
             string g = r["employee_gender"].ToString();
             this.gender = g[0];
 
-            this.approved = int.Parse(r["employee_approved"].ToString());
-            this.created_at = DateTime.Parse(r["employee_created_at"].ToString());
-            this.phone = r["employee_phone"].ToString();
+            // The approved status is data of type bool. 
+            string approvedStatus = r["employee_approved"].ToString();
+            this.approved = (approvedStatus == "True");
+
+
 
             // Preparing the variables for the Job constructor
             int workingStatus = int.Parse(r["employee_working_status"].ToString());
@@ -80,9 +100,128 @@ namespace HR
             int hoursPerDay = int.Parse(r["employee_hoursPerDay"].ToString());
             string firstDay = r["employee_firstDay"].ToString();
 
-            // Setting the objects
+            // Setting the job object
             this.job = new Job(workingStatus,contract,hoursPerDay,firstDay);
+
+
+            // Preparing the varirables for the address constructor
+            string address1 = r["employee_address1"].ToString();
+            string address2 = r["employee_address2"].ToString();
+            string city = r["employee_city"].ToString();
+            string state = r["employee_state"].ToString();
+            int zipCode = int.Parse(r["employee_zip_code"].ToString());
+            this.address = new Address(address1, address2, city, state, zipCode);
+
         }
+
+
+        /**
+         * create
+         */
+        public bool create() {
+            if (this.id == null) {
+                DatabaseHandler handler = new DatabaseHandler();
+
+                string insertSt = "INSERT INTO Employee (employee_id, employee_SSN, employee_firstName, employee_middleInital, employee_lastName, employee_dob, employee_working_status, employee_contract, employee_hoursPerDay, employee_firstDay, employee_gender, employee_position, employee_approved, employee_created_at, employee_address1, employee_address2, employee_city, employee_state, employee_zip_code, employee_phone) VALUES        (NULL,@SSN,@firstName,@middleInitial,@lastName,@DOB,@workingStatus,@contract,@hoursPerDay,@firstDay,@gender,@position,@approved,@created_at,@address1,@address2,@city,@state,@zipCode,@phone)";
+                handler.setSQL(insertSt);
+
+                // TO DO .. Working on the parameters
+
+            }
+
+            return false;
+        }
+
+
+        //----------------------- GETTERS ----------------------\\
+        public string getFirstName() {
+            return this.firstName;
+        }
+
+        public string getSSN() {
+            return this.SSN;
+        }
+
+        public int getID() {
+            return this.id;
+        }
+
+        public char getMiddleInitial() {
+            return this.middleInitial;
+        }
+
+        public string getLastName() {
+            return this.lastName;
+        }
+
+        public DateTime getDob() {
+            return this.dob;
+        }
+
+        public char getGender() {
+            return this.gender;
+        }
+
+        public bool getApprovedStatus() {
+            return this.approved;
+        }
+
+        public DateTime getCreatedDateAndTime() {
+            return this.created_at;
+        }
+
+        public string getPhone() {
+            return this.phone;
+        }
+
+        public Address getAddress() {
+            return this.address;
+        }
+
+        public Job getJob() {
+            return this.job;
+        }
+
+
+        //----------------------- SETTERS ----------------------\\
+        // WE DON't NEED TO SET the created_at and the id or the approved
+
+        public void setFirstName(string firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setMiddleInitial(char m) {
+            this.middleInitial = m;
+        }
+
+        public void setLastName(string lastName) {
+            this.lastName = lastName;
+        }
+
+        public void setSSN(string SSN) {
+            this.SSN = SSN;
+        }
+
+        public void setDob(DateTime dob) {
+            this.dob = dob;
+        }
+
+        public void setGender(char g) {
+            this.gender = g;
+        }
+
+        public void setPhone(string phone) {
+            this.phone = phone;
+        }
+
+        public void setJob(Job job) {
+            this.job = job;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
 
     }
 }

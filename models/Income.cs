2791e869480ae08a84da89b541da8877b7423000
othmanA <abah4506@stthomas.edu>
2013-11-28 15:17:34 -0600

    Creating the Income Model

--- models/Income.cs
+++ models/Income.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
-namespace HR_SYSTEM.models
+using HRDatabase;
+namespace HR
 {
     public class Income
     {
-        private String type{get; set;}
-        private float amount{get; set;}
-        private String per{get; set;}
+        private int id;
+        private string type;
+        private string per;
+        private float amount;
 
+        public Income(int id, string type, string per, float amount) { 
+            this.id = id;
+            this.type = type;
+            this.per = per;
+            this.amount = amount;
+        }
+
+        public static int create(int employee_id, string type, string per, float amount) {
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.setSQL("INSERT INTO Income (employee, income_type, income_amount, income_per) VALUES (@employee_id,@type,@amount,@per)");
+
+            handler.addParameter("@employee_id", employee_id.ToString());
+            handler.addParameter("@type", amount.ToString());
+            handler.addParameter("@amount", type);
+            handler.addParameter("@per", per);
+
+
+            return handler.ExecuteNonQuery();
+        }
+
+        public int delete()
+        {
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.setSQL("DELETE FROM Income WHERE income_id = @id");
+            handler.addParameter("@id", this.id.ToString());
+            return handler.ExecuteNonQuery();
+        }
+
+
+        public int Id{
+            get {return id;}
+        }
+
+        public string Type{
+            get {return type;}
+        }
+
+        public string Per{
+            get {return per;}
+        }
+
+        public float Amount{
+            get{return amount;}
+        }
         
     }
 }

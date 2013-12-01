6cb225b24e8147c31dee0090eefb6fca80435dab
othmanA <abah4506@stthomas.edu>
2013-12-01 08:22:31 -0600

    Updating the position and department Models

--- models/Department.cs
+++ models/Department.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Web;
-
-namespace HR_SYSTEM.models
+using HRDatabase;
+namespace HR
 {
     public class Department
     {
-        private String name {get; set;}
+        private int id;
+        private string name;
+
+        public Department(int id, string name) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public Department(int id) {
+            this.id = id;
+            this.init();
+        }
+
+        private void init(){
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.setSQL("SELECT * FROM [Department] WHERE department_id = @id");
+            handler.addParameter("@id",this.id.ToString());
+            handler.queryExecute();
+            while (handler.reader.Read()) {
+                this.name = handler.reader["department_name"].ToString();
+            }
+        }
+
+        public static ArrayList getALL() {
+            ArrayList departments = new ArrayList();
+
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.setSQL("SELECT * FROM [Department] ORDER BY department_name");
+            handler.queryExecute();
+
+            while (handler.reader.Read()) {
+
+                string department_name = handler.reader["department_name"].ToString();
+                int department_id = int.Parse(handler.reader["department_id"].ToString());
+
+                Department d = new Department(department_id,department_name);
+                departments.Add(d);
+            }
+
+            return departments;
+        }
+
+
+
+        public static int create(string department_name) {
+            DatabaseHandler handler = new DatabaseHandler();
+
+            handler.setSQL("INSERT INTO [Department] (department_name) VALUES (@name)");
+            handler.addParameter("@name", department_name);
+            
+            return handler.ExecuteNonQuery();
+        }
     }
 }
--- models/Position.cs
+++ models/Position.cs
@@ -7,27 +7,31 @@ namespace HR
 {
     public class Position
     {
-        private String name;
+        private string name;
         private int id;
+        private Department department;
 
         public Position(int positionID) {
             this.id = positionID;
             this.init();
         }
 
-        public Position(int id, string name) {
+        public Position(int id, string name, Department department) {
             this.id = id;
             this.name = name;
+            this.department = department;
         }
 
         private void init(){
             if (this.id != 0) {
                 DatabaseHandler handler = new DatabaseHandler();
-                handler.setSQL("SELECT position_name FROM Position WHERE position_id = @id");
+                handler.setSQL("SELECT * FROM Position WHERE position_id = @id");
                 handler.addParameter("@id", this.id.ToString());
                 handler.queryExecute();
                 while (handler.reader.Read()) {
                     this.name = handler.reader["position_name"].ToString();
+                    int dep_id = int.Parse(handler.reader["position_department"].ToString());
+                    Department d = new Department(dep_id);
                 }
             }
         }
@@ -42,8 +46,13 @@ namespace HR
             set { name = value; } 
         }
 
+        public Department Department {
+            get { return department; }
+            private set { department = value; }
+        }
+
         /// <summary>
-        /// This will return an arraylist contain the name and id for each 
+        /// This will return an arraylist that contains the name and id and the department for each 
         /// </summary>
         /// <returns></returns>
         public static ArrayList getALL() {
@@ -56,7 +65,8 @@ namespace HR
 
                 int id = int.Parse(handler.reader["position_id"].ToString());
                 string name = handler.reader["position_name"].ToString();
-                Position p = new Position(id,name);
+                int department_id = int.Parse(handler.reader["position_department"].ToString());
+                Position p = new Position(id,name,new Department(department_id));
                 list.Add(p);
             }
 

fc4713a71581d054583058f3f4e7cec9339d63a6
othmanA <abah4506@stthomas.edu>
2013-12-03 14:05:55 -0600

    UPLOAD to C:\\upload\  --- Read the description
    In this commit we are now uploading files to the system. because we have
    two computers the path of the upload file will not be the same. So I did
    it using the root directory C
    
    Create a folder called upload in the C drive then try to upload a
    document and see if the file uploaded or not.

---
+++ views/EmployeeAddDocument.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeAddDocument.aspx.cs" Inherits="HR_SYSTEM.views.EmployeeAddDocument" %>
+
+<%@ Register src="header.ascx" tagname="header" tagprefix="uc1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        
+        <uc1:header ID="header2" runat="server" />
+        <div class="container">
+        <div class="well">
+            
+            <h3>
+                Add Document
+            </h3>
+
+            <table class="table" style="border:0">
+		<tr>
+			<td>Name: </td>
+            <td>
+                <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            </td>
+		</tr>
+
+		<tr>
+			<td>File: </td>
+            <td>
+                
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+                
+            </td>
+		</tr>
+
+        
+
+		<tr>
+			<td colspan="2">
+				
+				
+			    <br />
+                <asp:Button ID="Button1" runat="server" CssClass="btn btn-success" 
+                     Text="Add" onclick="Button1_Click" />
+				
+				
+			</td>
+		</tr>
+        </table>
+        </div>
+        
+        </div>
+    </div>
+    
+    </form>
+</body>
+</html>
+
---
+++ views/EmployeeAddDocument.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HR;
+namespace HR_SYSTEM.views
+{
+    public partial class EmployeeAddDocument : System.Web.UI.Page
+    {
+        Employee employee = new Employee();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get the id of the employee from the url and pass it to the employee object
+            // Also we are saving the object in the class because we need to use it in all the moethds.
+            try
+            {
+                this.employee.findById(int.Parse(Request.QueryString["id"]));
+            }
+            catch (Exception exce)
+            {
+                Response.Redirect("Employees.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // THIS can not work during the devolpment stage.
+            try
+            {
+                string name = nameTextBox.Text;
+                string path = FileUpload1.FileName;
+                FileUpload1.SaveAs("C:\\upload\\" + FileUpload1.FileName.ToString());
+                int size = FileUpload1.PostedFile.ContentLength;
+                string note = "";
+                int check = Document.create(employee.Id, name, path, size, note);
+                if (check == 1)
+                {
+                    header2.success("Document Added");
+                }
+                else {
+                    header2.showAlert("Something went WRong!");
+                }
+            }
+            catch (Exception ex) {
+                header2.showAlert(ex.Message);
+            }
+            
+        }
+    }
+}

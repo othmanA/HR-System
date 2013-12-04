57eef0bcc9a2f67e201e6817d527923dd1ad0a85
othmanA <abah4506@stthomas.edu>
2013-12-04 13:05:22 -0600

    showing the departments

--- views/departments.aspx
+++ views/departments.aspx
@@ -1,11 +1,12 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="HR_SYSTEM.views.Employees" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="departments.aspx.cs" Inherits="HR_SYSTEM.views.departments" %>
 
 <%@ Register src="header.ascx" tagname="header" tagprefix="uc1" %>
 
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
+<head id="Head1" runat="server">
     <title></title>
 </head>
 <body>
@@ -16,7 +17,7 @@
 
         <div class="container">
         <div class="well">
-        <h4>Employees</h4>
+        <h4>Departments</h4>
 
 
             <asp:Table ID="Table1" runat="server" CssClass="table">
---
+++ views/departments.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HR;
+namespace HR_SYSTEM.views
+{
+    public partial class departments : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Creating a row
+            TableRow headerRow = new TableRow();
+
+            // Creating the cells
+            TableHeaderCell idHeader = new TableHeaderCell();
+            TableHeaderCell nameHeader = new TableHeaderCell();
+            TableHeaderCell editHeader = new TableHeaderCell();
+            TableHeaderCell deleteHeader = new TableHeaderCell();
+
+            // Adding the text to each cell
+            idHeader.Text = "ID#";
+            nameHeader.Text = "Name";
+            editHeader.Text = "Edit";
+            deleteHeader.Text = "Delete";
+
+
+            // add the cells to the row
+            headerRow.Cells.Add(idHeader);
+            headerRow.Cells.Add(nameHeader);
+            headerRow.Cells.Add(editHeader);
+            headerRow.Cells.Add(deleteHeader);
+
+            // Add the row to the table
+            Table1.Rows.Add(headerRow);
+
+
+            // Arraylist of departments
+            ArrayList deps = new ArrayList();
+            deps = Department.getALL();
+
+            foreach(Department d in deps){
+                TableRow row = new TableRow();
+
+                TableCell id = new TableCell();
+                TableCell name = new TableCell();
+                TableCell edit = new TableCell();
+                TableCell delete = new TableCell();
+
+                id.Text = d.ID.ToString();
+                name.Text = d.Name;
+                edit.Text = "Edit";
+                delete.Text = "Delete";
+
+                row.Cells.Add(id);
+                row.Cells.Add(name);
+                row.Cells.Add(edit);
+                row.Cells.Add(delete);
+
+                Table1.Rows.Add(row);
+            }
+
+            Table1.CssClass = "table";
+
+        }
+    }
+}
